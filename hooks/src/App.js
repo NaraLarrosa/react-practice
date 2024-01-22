081c3f062f62933ddb86c245d3ae163c1615b84a
Nara Larrosa <naranatalia.larrosa@gmail.com>
2024-01-22 16:53:59 -0300

    Add exercise hooks

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,25 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
 const items = [
   {
-    title: 'Click aqui!! :)',
-    content: 'Si funciono ;)',
+    title: 'Click aqui!',
+    content: 'Si funciono! ;)',
   },
   {
-    title: 'No entres aca',
-    content: 'Dije que no! u.u',
+    title: 'No ingreses aqui',
+    content: 'Dije que no! :p',
   },
   {
-    title: 'Prueba prueba',
-    content: 'Prueba',
+    title: 'Prueba',
+    content: 'Probando probando... :)',
+  },
+];
+
+const options = [
+  {
+    label: 'This is Red',
+    value: 'red',
+  },
+  {
+    label: 'This is Green',
+    value: 'green',
+  },
+  {
+    label: 'This is Blue',
+    value: 'blue',
   },
 ];
 
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <Accordion items={items} />
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
