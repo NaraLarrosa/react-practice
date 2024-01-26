6f83e59b6d3c48e72b9390bbd9a56cfcda1e03df
Nara Larrosa <naranatalia.larrosa@gmail.com>
2024-01-26 17:30:43 -0300

    Add books api with redux

---
+++ redux-simple-starter/src/reducers/index.js
@@ -0,0 +1,16 @@
+// import { combineReducers } from 'redux';
+
+// const rootReducer = combineReducers({
+//   state: (state = {}) => state
+// });
+
+// export default rootReducer;
+
+import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books'
+
+const rootReducer = combineReducers({
+  books: BooksReducer
+});
+
+export default rootReducer;
