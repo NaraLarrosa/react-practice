811b448017412dd4f0faef2fd4258a4704a73920
Nara Larrosa <naranatalia.larrosa@gmail.com>
2024-01-29 16:14:33 -0300

    React with Redux

--- redux-simple-starter/src/reducers/index.js
+++ redux-simple-starter/src/reducers/index.js
@@ -1,16 +1,10 @@
-// import { combineReducers } from 'redux';
-
-// const rootReducer = combineReducers({
-//   state: (state = {}) => state
-// });
-
-// export default rootReducer;
-
 import { combineReducers } from 'redux';
-import BooksReducer from './reducer_books'
+import BooksReducer from './reducer_books';
+import ActiveBook from './reducer_active_book';
 
 const rootReducer = combineReducers({
-  books: BooksReducer
+  books: BooksReducer,
+  ActiveBook: ActiveBook
 });
 
 export default rootReducer;
